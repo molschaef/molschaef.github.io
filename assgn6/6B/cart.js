3a230a62f6e38762b675c610c19ff39e09527880
Molly Schaefer <mollysch@andrew.cmu.edu>
2019-10-26 21:55:04 -0400

    Update cart.js

--- assgn6/6B/cart.js
+++ assgn6/6B/cart.js
@@ -4,20 +4,25 @@ let products = JSON.parse(localStorage.getItem("cart"));
 function updateCart() {
     let htmlString = "";
     let itemNum = 0;
+    // store total price
+    let total = 0;
     // loop through each product and add that item to the html string
     if (products.length==0) {
             document.getElementById("cart-container").innerHTML = "<div></div><div id=\"nothing\">There is nothing in your cart!</div>"
     } else {
         for (let product of products) {
+            // set product id to use for remove function
             product.id = itemNum;
+            // add product to html string
             htmlString += `<div class=\"cart-item\"><img src =\"${product.prodImg}\" alt=\"${product.flavor}\"></div><div class=\"cart-item\"><p>${product.flavor}<br/><br/>Glaze: ${product.glaze}<br/>Quantity: ${product.quant}<br/></p></div><div>${product.price}</div><div class = \"cart-item\"><a href=\"\" onclick = "removeItem(${product.id})">Remove</a></div>`;
             itemNum++;
+            // add the current product price to the total by removing the $ and converting the string to an int
+            total += parseInt(product.price.substr(1));
         }
         // add the html string of products to the grid
-        document.getElementById("cart-container").innerHTML = "<div></div><div></div><div></div><div><button type=\"button\">Checkout</button></div>" + htmlString + "<div></div><div></div><div></div><div><button type=\"button\">Checkout</button></div>";
+        document.getElementById("cart-container").innerHTML = "<div></div><div></div><div></div><div><button type=\"button\">Checkout</button></div>" + htmlString + `<div></div><div></div><div id = \"total\">Total: \$${total}.00</div><div></div>` + "<div></div><div></div><div></div><div><button type=\"button\">Checkout</button></div>";
     }
 };
-
 
 // remove an item from the shopping cart
 function removeItem(productId) {
