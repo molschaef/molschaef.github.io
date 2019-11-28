94267dd453be7a541232c2111ddcc78c9dc2aa1e
Molly Schaefer <mollysch@andrew.cmu.edu>
2019-11-28 13:55:25 -0500

    update nav, add bootstrap

---
+++ assets/js/emotion.js
@@ -0,0 +1,15 @@
+// Prototype for emotions
+function Emotion(name, description, example){
+    this.name = name,
+    this.description = description,
+    this.example = example
+};
+
+var happy = new Emotion("happy", "sample text", "sample text");
+var sad = new Emotion("sad", "sample text", "sample text");
+var loved = new Emotion("loved", "sample text", "sample text");
+var lonely = new Emotion("lonely", "sample text", "sample text");
+var brave = new Emotion("brave", "sample text", "sample text");
+var angry = new Emotion("angry", "sample text", "sample text");
+var peaceful = new Emotion("peaceful", "sample text", "sample text");
+var overwhelmed = new Emotion("overwhelmed", "sample text", "sample text");
