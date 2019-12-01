722c222409f32d336de4bdf1f2208ae34002015f
Molly Schaefer <mollysch@andrew.cmu.edu>
2019-11-30 20:18:44 -0500

    update emotion page

--- assets/js/emotion.js
+++ assets/js/emotion.js
@@ -1,28 +1,37 @@
 // Prototype for emotions
-function Emotion(name, description, example){
+function Emotion(name, description, example, img){
     this.name = name,
     this.description = description,
-    this.example = example
+    this.example = example,
+    this.img = img
 };
 
-var happy = new Emotion("happy", "sample text", "sample text");
-var sad = new Emotion("sad", "sample text", "sample text");
-var loved = new Emotion("loved", "sample text", "sample text");
-var lonely = new Emotion("lonely", "sample text", "sample text");
-var brave = new Emotion("brave", "sample text", "sample text");
-var angry = new Emotion("angry", "sample text", "sample text");
-var peaceful = new Emotion("peaceful", "sample text", "sample text");
-var overwhelmed = new Emotion("overwhelmed", "sample text", "sample text");
+var happy = new Emotion("Happy", "sample text", "sample text", "");
+var sad = new Emotion("Sad", "sample text", "sample text", "assets/images/sad.png");
+var loved = new Emotion("Loved", "sample text", "sample text", "");
+var lonely = new Emotion("Lonely", "sample text", "sample text", "");
+var brave = new Emotion("Brave", "sample text", "sample text", "");
+var mad = new Emotion("Mad", "sample text", "sample text", "");
+var peaceful = new Emotion("Peaceful", "sample text", "sample text", "../images/sad.png");
+var overwhelmed = new Emotion("Overwhelmed", "sample text", "sample text", "../images/sad.png");
+
+var impatient = new Emotion("Impatient", "sample text", "sample text", "sad.jpg");
+var curious = new Emotion("Curious", "sample text", "sample text", "sad.png");
+var excited = new Emotion("Excited", "sample text", "sample text", "../images/sad.png");
+var friendly = new Emotion("Friendly", "sample text", "sample text", "../images/sad.png");
+var shy = new Emotion("Shy", "sample text", "sample text", "../images/sad.png");
+var worried = new Emotion("Worried", "sample text", "sample text", "../images/sad.png");
+var silly = new Emotion("Silly", "sample text", "sample text", "../images/sad.png");
 
 
-var emotions = [happy, sad, loved, lonely, brave, angry, peaceful, overwhelmed];
+var emotions = [brave, curious, excited, friendly, happy, impatient, lonely, loved, mad, overwhelmed, peaceful, sad, shy, silly, worried];
 
 function createEmotions() {
     let htmlString = "";
     for (let emotion of emotions) {
         //  for reference: htmlString += `<div class=\"cart-item\"><img src =\"${product.prodImg}\" alt=\"${product.flavor}\"></div><div class=\"cart-item\"><p>${product.flavor}<br/><br/>Glaze: ${product.glaze}<br/>Quantity: ${product.quant}<br/></p></div><div>$${product.quant*parseInt(product.price.slice(1))}.00</div><div class = \"cart-item\"><a href=\"\" onclick = "removeItem(${product.id})">Remove</a></div>`;
-        htmlString += '<div class=\"col-sm-3 box\" id= \"' + emotion.name + '\">' + emotion.name + '</div>';
-        console.log(emotion.name);
+        // htmlString += '<div class=\"col-sm-3 box\" id= \"' + emotion.name + '\">' + emotion.name + '</div>';
+        htmlString += '<div class=\"flip-card col-sm-6 col-md-4\" id = \"' + emotion.name + '\"><div class=\"flip-card-inner col-sm-3\"><div class=\"flip-card-front\"><h1>' + emotion.name + '</h1><img src=\"' + emotion.img + '\" alt=\"' + emotion.name + '\" style=\"width:200px;height:200px;\"></div><div class=\"flip-card-back\"><h1>' + emotion.name + '</h1><p>' + emotion.description + '</p><p>' + emotion.example + '</p></div></div></div>';
     }
     document.getElementById("emotion-body").innerHTML = htmlString;
-}
+};
